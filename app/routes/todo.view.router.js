aeb46286a10ee0d1ada4dfeaa90c38d59957839b
Betul <betulkoru06@gmail.com>
2024-04-25 20:30:51 +0300

    routers adjusted

--- app/routes/todo.view.router.js
+++ app/routes/todo.view.router.js
@@ -6,13 +6,19 @@
 
 const todo = require('../controllers/todo.view.controller');
 
-const router = require('express').Router()
+const router = require('express').Router();
 
-router.get('/', todo.list);
+// router.get('/', todo.list);
+router.all('/', todo.list);
 
 // router.get('/create', todo.create);   // form view
 // router.post('/create', todo.create);    // form send
 router.all('create', todo.create);
 
+router.all('/:id', todo.read);
 
-module.exports = router
+router.all('/:id/update', todo.update);
+
+router.all('/:id/delete', todo.delete);
+
+module.exports = router;
